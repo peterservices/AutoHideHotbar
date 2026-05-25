00db2f28e65b684eb0cc72f651d1fae48beb3bbf
Ticataco <148506232+peterservices@users.noreply.github.com>
2026-05-25 14:02:17 -0600

    Add config and option to enable held item tooltips

--- src/client/java/com/github/peterservices/autohidehotbar/client/AutoHideHotbarClient.java
+++ src/client/java/com/github/peterservices/autohidehotbar/client/AutoHideHotbarClient.java
@@ -1,5 +1,8 @@
 package com.github.peterservices.autohidehotbar.client;
 
+import com.github.peterservices.autohidehotbar.client.config.AutoHideHotbarConfig;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
@@ -7,14 +10,18 @@ import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
 
 public final class AutoHideHotbarClient implements ClientModInitializer {
     public void onInitializeClient() {
+        AutoConfig.register(AutoHideHotbarConfig.class, JanksonConfigSerializer::new);
         HotbarStateTracker.init();
-        ClientTickEvents.END_CLIENT_TICK.register((client) -> StatChangeTracker.tick());
+        ClientTickEvents.END_CLIENT_TICK.register(StatChangeTracker::tick);
         HudElementRegistry.replaceElement(VanillaHudElements.HOTBAR, (original) -> (ctx, tick) -> {
             if (HotbarStateTracker.shouldRender()) {
                 original.extractRenderState(ctx, tick);
             }
         });
         HudElementRegistry.replaceElement(VanillaHudElements.HELD_ITEM_TOOLTIP, (original) -> (ctx, tick) -> {
+            if (!AutoHideHotbarConfig.getInstance().hideHeldItemTooltips) {
+                original.extractRenderState(ctx, tick);
+            }
         });
         HudElementRegistry.replaceElement(VanillaHudElements.INFO_BAR, (original) -> (ctx, tick) -> {
             if (StatChangeTracker.shouldShowExperience()) {
