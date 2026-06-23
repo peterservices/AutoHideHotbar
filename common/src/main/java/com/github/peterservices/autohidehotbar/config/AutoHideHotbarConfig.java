e3692e0239d6239d241ec9431beec36d79ae5f9c
Ticataco <148506232+peterservices@users.noreply.github.com>
2026-06-23 11:25:03 -0600

    config: Adjust default config for better initial experience * Only hide health and hunger when full by default * Increase visible times for all HUD elements

--- common/src/main/java/com/github/peterservices/autohidehotbar/config/AutoHideHotbarConfig.java
+++ common/src/main/java/com/github/peterservices/autohidehotbar/config/AutoHideHotbarConfig.java
@@ -6,11 +6,11 @@ public class AutoHideHotbarConfig extends MidnightConfig {
     public static final String GENERAL = "general";
     public static final String TIMINGS = "timings";
 
-    @Entry(category = GENERAL) public static boolean hideHeldItemTooltips = true;
+    @Entry(category = GENERAL) public static boolean hideHeldItemTooltips = false;
     @Entry(category = GENERAL) public static boolean useCustomStatsDisplay = false;
     @Comment(category = GENERAL) public static Comment spacer1;
-    @Entry(category = GENERAL) public static boolean onlyHideWhenFullHealth = false;
-    @Entry(category = GENERAL) public static boolean onlyHideWhenFullFood = false;
+    @Entry(category = GENERAL) public static boolean onlyHideWhenFullHealth = true;
+    @Entry(category = GENERAL) public static boolean onlyHideWhenFullFood = true;
     @Comment(category = GENERAL) public static Comment spacer2;
     @Entry(category = GENERAL) public static boolean neverHideHotbar = false;;
     @Entry(category = GENERAL) public static boolean neverHideHealth = false;
@@ -18,10 +18,10 @@ public class AutoHideHotbarConfig extends MidnightConfig {
     @Entry(category = GENERAL) public static boolean neverHideArmor = false;
     @Entry(category = GENERAL) public static boolean neverHideExperience = false;
 
-    @Comment(category = TIMINGS, centered = true) public static Comment timingsComment;
-    @Entry(category = TIMINGS, min = 0) public static int hotbarShowMilliseconds = 1000;
+    @Entry(category = TIMINGS, min = 0) public static int hotbarShowMilliseconds = 1500;
     @Entry(category = TIMINGS, min = 0) public static int healthShowTicks = 40;
-    @Entry(category = TIMINGS, min = 0) public static int foodShowTicks = 25;
-    @Entry(category = TIMINGS, min = 0) public static int armorShowTicks = 25;
-    @Entry(category = TIMINGS, min = 0) public static int experienceShowTicks = 25;
+    @Entry(category = TIMINGS, min = 0) public static int foodShowTicks = 30;
+    @Entry(category = TIMINGS, min = 0) public static int armorShowTicks = 30;
+    @Entry(category = TIMINGS, min = 0) public static int experienceShowTicks = 30;
+    @Comment(category = TIMINGS, centered = true) public static Comment timingsComment;
 }
