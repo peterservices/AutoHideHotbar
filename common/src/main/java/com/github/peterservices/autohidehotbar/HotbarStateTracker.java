8aa1a13c48256d118dc5e6b07a806b5e378f81c5
fhoelz <kachelstacktus@gmail.com>
2026-07-20 17:02:11 +0200

    Hotbar becomes visible when it is updated inside inventory screen

--- common/src/main/java/com/github/peterservices/autohidehotbar/HotbarStateTracker.java
+++ common/src/main/java/com/github/peterservices/autohidehotbar/HotbarStateTracker.java
@@ -1,11 +1,22 @@
 package com.github.peterservices.autohidehotbar;
 
+import java.util.Arrays;
+
 import com.github.peterservices.autohidehotbar.config.AutoHideHotbarConfig;
 import dev.architectury.event.events.client.ClientTickEvent;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.gui.screens.inventory.AbstractFurnaceScreen;
+import net.minecraft.client.gui.screens.inventory.AbstractMountInventoryScreen;
+import net.minecraft.client.gui.screens.inventory.InventoryScreen;
+import net.minecraft.world.item.Item;
 
 public class HotbarStateTracker {
     private static int lastSlot = -1;
     private static long lastChangeTime = 0L;
+    private static Item[] lastHotbarItems = new Item[9];
 
     public static void init() {
         ClientTickEvent.CLIENT_POST.register((client) -> {
@@ -14,6 +25,14 @@ public class HotbarStateTracker {
                 if (current != lastSlot) {
                     lastSlot = current;
                     lastChangeTime = System.currentTimeMillis();
+                }
+
+                if (isInInventoryUI()) {
+                    Item[] hotbarItems = getHotbarItems(client);
+                    if (!Arrays.equals(hotbarItems, lastHotbarItems)) {
+                        lastHotbarItems = hotbarItems;
+                        lastChangeTime = System.currentTimeMillis();
+                    }
                 }
             }
         });
@@ -25,5 +44,21 @@ public class HotbarStateTracker {
 
     public static boolean shouldRender() {
         return AutoHideHotbarConfig.neverHideHotbar || shouldShowHotbarRaw();
+    }
+
+    private static boolean isInInventoryUI() {
+        Screen screen = Minecraft.getInstance().gui.screen();
+        return (screen instanceof InventoryScreen 
+            || screen instanceof AbstractMountInventoryScreen 
+            || screen instanceof AbstractContainerScreen
+            || screen instanceof AbstractFurnaceScreen);
+    }
+
+    private static Item[] getHotbarItems(Minecraft client) {
+        Item[] hotbarItems = new Item[9];
+        for (int i = 0; i < 9; i++) {
+            hotbarItems[i] = client.player.getInventory().getSlot(i).get().getItem();
+        }
+        return hotbarItems;
     }
 }
