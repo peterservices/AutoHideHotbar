29c220063426986dcfa65cc3cbd64367100d49a7
Ticataco <148506232+peterservices@users.noreply.github.com>
2026-07-21 14:22:55 -0600

    feat: Add more fine controls for locator bar display mode * Fix opacity always being applied to locator bar

--- common/src/main/java/com/github/peterservices/autohidehotbar/mixin/LocatorBarMixin.java
+++ common/src/main/java/com/github/peterservices/autohidehotbar/mixin/LocatorBarMixin.java
@@ -11,26 +11,34 @@ import net.minecraft.util.ARGB;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.util.List;
+
 @Mixin(LocatorBar.class)
 public class LocatorBarMixin {
     @WrapOperation(method = "extractBackground", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphicsExtractor;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/Identifier;IIII)V"))
     private void onExtractBackground(GuiGraphicsExtractor instance, RenderPipeline renderPipeline, Identifier location, int x, int y, int width, int height, Operation<Void> original) {
-        if (AutoHideHotbarConfig.locatorBarDisplayMode == AutoHideHotbarConfig.LocatorBarDisplayMode.SHOW) {
+        if (AutoHideHotbarConfig.locatorBarDisplayMode == AutoHideHotbarConfig.LocatorBarDisplayMode.OPACITY) {
             instance.blitSprite(renderPipeline, location, x, y, width, height, ARGB.white(AutoHideHotbarConfig.hotbarStatsOpacity));
+        } else if (AutoHideHotbarConfig.locatorBarDisplayMode == AutoHideHotbarConfig.LocatorBarDisplayMode.SHOW) {
+            original.call(instance, renderPipeline, location, x, y, width, height);
         }
     }
 
     @WrapOperation(method = "lambda$extractRenderState$1(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/waypoints/PartialTickSupplier;Lnet/minecraft/client/gui/GuiGraphicsExtractor;ILnet/minecraft/world/waypoints/TrackedWaypoint;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphicsExtractor;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/Identifier;IIIII)V"))
     private void onExtractRenderStateWaypoint(GuiGraphicsExtractor instance, RenderPipeline renderPipeline, Identifier location, int x, int y, int width, int height, int color, Operation<Void> original) {
-        if (AutoHideHotbarConfig.locatorBarDisplayMode != AutoHideHotbarConfig.LocatorBarDisplayMode.HIDE) {
+        if (List.of(AutoHideHotbarConfig.LocatorBarDisplayMode.OPACITY, AutoHideHotbarConfig.LocatorBarDisplayMode.OPACITYANDNOBACKGROUND).contains(AutoHideHotbarConfig.locatorBarDisplayMode)) {
             instance.blitSprite(renderPipeline, location, x, y, width, height, ARGB.multiplyAlpha(color, AutoHideHotbarConfig.hotbarStatsOpacity));
+        } else if (List.of(AutoHideHotbarConfig.LocatorBarDisplayMode.SHOW, AutoHideHotbarConfig.LocatorBarDisplayMode.NOBACKGROUND).contains(AutoHideHotbarConfig.locatorBarDisplayMode)) {
+            original.call(instance, renderPipeline, location, x, y, width, height, color);
         }
     }
 
     @WrapOperation(method = "lambda$extractRenderState$1(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/waypoints/PartialTickSupplier;Lnet/minecraft/client/gui/GuiGraphicsExtractor;ILnet/minecraft/world/waypoints/TrackedWaypoint;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphicsExtractor;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/Identifier;IIII)V"))
     private void onExtractRenderStateArrow(GuiGraphicsExtractor instance, RenderPipeline renderPipeline, Identifier location, int x, int y, int width, int height, Operation<Void> original) {
-        if (AutoHideHotbarConfig.locatorBarDisplayMode != AutoHideHotbarConfig.LocatorBarDisplayMode.HIDE) {
+        if (List.of(AutoHideHotbarConfig.LocatorBarDisplayMode.OPACITY, AutoHideHotbarConfig.LocatorBarDisplayMode.OPACITYANDNOBACKGROUND).contains(AutoHideHotbarConfig.locatorBarDisplayMode)) {
             instance.blitSprite(renderPipeline, location, x, y, width, height, ARGB.white(AutoHideHotbarConfig.hotbarStatsOpacity));
+        } else if (List.of(AutoHideHotbarConfig.LocatorBarDisplayMode.SHOW, AutoHideHotbarConfig.LocatorBarDisplayMode.NOBACKGROUND).contains(AutoHideHotbarConfig.locatorBarDisplayMode)) {
+            original.call(instance, renderPipeline, location, x, y, width, height);
         }
     }
 }
